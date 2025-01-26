45bcdc7797d2942af80a358759d6ccf198b62760
AWERDdev <kokorami565@gmail.com>
2025-01-26 22:40:36 +0200

    final touch

--- client/src/Pages/Signup.jsx
+++ client/src/Pages/Signup.jsx
@@ -59,7 +59,7 @@ const InputHandling = async ()=>{
 }
 const SendData = async ()=>{
     try{
-        const response = await fetch(`http://localhost:3500/signup`,{
+        const response = await fetch(`https://stockMarket-backend.vercel.app/signup`,{
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
--- client/src/Pages/login.jsx
+++ client/src/Pages/login.jsx
@@ -36,7 +36,7 @@ function LoginPage(){
     }
     const SendData = async ()=>{
         try{
-            const response = await fetch(`http://localhost:3500/login`,{
+            const response = await fetch(`https://stockMarket-backend.vercel.app/login`,{
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
